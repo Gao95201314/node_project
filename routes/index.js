cfb459defd94d9074a814c3c411423abc255dbe1
Your Name <you@example.com>
2018-11-24 06:53:19 +0800

    '把首页基本建立起来了'

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' }); //index.ejs文件后缀可省略
 });
-
+router.get('/users.html', function(req, res, next) {
+    res.render('users');
+});
+router.get('/grand.html', function(req, res, next) {
+    res.render('grand');
+});
+router.get('/phone.html', function(req, res, next) {
+    res.render('phone');
+});
 module.exports = router;
