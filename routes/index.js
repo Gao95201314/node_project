7b968f2b02d72f0745971e0d69056e9cae62f461
Your Name <you@example.com>
2018-11-24 16:58:40 +0800

    '把登录注册以及手机品牌页面写了'

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    res.render('login');
+});
+router.get('/register.html', function(req, res, next) {
+    res.render('register');
+});
+router.get('/index.html', function(req, res, next) {
     res.render('index', { title: 'Express' }); //index.ejs文件后缀可省略
 });
 router.get('/users.html', function(req, res, next) {
